606da15e6a940d087a886ff1cc1df28d866a60fa
Tottolush <fargoodingmail.com>
2026-03-31 23:06:52 -0300

    fix port

--- server.py
+++ server.py
@@ -98,4 +98,6 @@ async def websocket_endpoint(ws: WebSocket, room_id: str, player_name: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
